58092be273fd1fe1dd2b9f7de5f84751d8dbdcde
Leonard Luangga Kekang <leonardkekang@gmail.com>
2015-03-19 16:42:01 +0800

    Revert "ddddd"
    This reverts commit 630ce6bfefa42076f80f8fed0801980beb0cd537.

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('username')->unique();
+			$table->string('email')->unique();
+			$table->string('password', 60);
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+<<<<<<< HEAD
+	//halo
+=======
+>>>>>>> 9e3039d85a68b738a2ce589efdab36e64c945606
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
