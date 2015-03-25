8771c75e0f8e633d308eca02a0cbd8e947172bef
Zaenal Abidin <aenal.abie@gmail.com>
2015-03-25 10:28:58 +0800

    coba merubah grid

--- Modules/Jurusan/Resources/views/tahun_akademik/create.blade.php
+++ Modules/Jurusan/Resources/views/tahun_akademik/create.blade.php
@@ -2,14 +2,27 @@
 
 @section('content')
 
-	<?php
-	$data->add('kode_tahun_akademik','Title', true);
-	$data->add('tahun_akademik','author', true);
-	$data->edit('/tahun_akademik', 'Edit','modify|delete', 'kode_tahun_akademik');
-	$data->link('/tahun_akademik',"Add New", "TR");
-	$data->paginate(10);
-	?>
+<div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Data Tahun Akademik</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+					<?php
+					$data->add('kode_tahun_akademik','Title', true);
+					$data->attributes=array("class" => "table table-bordered");
+					$data->add('tahun_akademik','author', true);
+					$data->edit('/tahun_akademik', 'Edit','modify|delete', 'kode_tahun_akademik');
+					$data->link('/tahun_akademik',"Add New", "TR");
+					$data->paginate(10);
+					?>
 
-	{!! $data  !!}
+					{!! $data  !!}
+				</div>
+				</div>
+			</div>
+</div>
+
 
 @stop
